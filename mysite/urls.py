de1b685505149345a0af204e631f5a4d1942a796
Ramani-Vadaparthi <ramanivadaparthi@gmail.com>
2021-08-17 14:01:46 +0530

    _

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^reviews/', include('reviews.urls', namespace="reviews")),
+    url(r'', include('reviews.urls', namespace="reviews")),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls', namespace="auth")),
